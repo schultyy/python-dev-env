b84cdaf2b331478e78dacf8da2820212281c7b67
Jan Schulte <hello@unexpected-co.de>
2015-01-26 22:23:43 +0100

    Add postgresql configuration

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,12 @@
 node default {
  include git 
+ include postgresql::server
 }
+
+class { 'postgresql::server':
+  ip_mask_deny_postgres_user => '0.0.0.0/32',
+  ip_mask_allow_all_users    => '0.0.0.0/0',
+  listen_addresses           => '*',
+  postgres_password          => 'postgres',
+}
+
